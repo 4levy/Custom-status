740a218546f82cd7b26e01a56e8eaa83a96404f6
Levy <100963276+4levy@users.noreply.github.com>
2024-05-10 17:36:35 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const { Launcher } = require("@loybung/launcher");
+const { resolve } = require("path");
+const express = require("express");
+const cron = require("node-cron");
+const starting = express();
+const port = 3000;
+
+
+starting.get('/', (req, res) => {
+  res.redirect('http://4levy.xyz/');
+});
+
+starting.listen(port, () => {
+  console.log(`🔗 Listening to port : http://localhost:${port}`);
+});
+
+const app = new Launcher("https://loybung.vercel.app/api/project/customstatus");
+app.setPath(resolve(__dirname, "./app.js"));
+app.setExpire(null);
+
+function startApp() {
+  app.Run().catch((err) => {
+    console.error(err.message);
+    console.log("Restarting the application...");
+    startApp();
+  });
+}
+
+startApp();
+
+process.on('uncaughtException', (err) => {
+  console.error('Uncaught Exception:', err);
+  console.log("Restarting the application due to an uncaught exception...");
+  startApp();
+});
+
+cron.schedule("0 */10 * * *", () => {
+  console.log("Restarting the application...");
+  startApp();
+});
+se
