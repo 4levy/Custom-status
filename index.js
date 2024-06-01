804c47cef278f88d363cde1d06786df1c68e1d6c
Levy <100963276+4levy@users.noreply.github.com>
2024-06-01 16:49:04 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,41 +1,17 @@
 const { Launcher } = require("@loybung/launcher");
 const { resolve } = require("path");
 const express = require("express");
-const cron = require("node-cron");
+
 const starting = express();
 const port = 3000;
 
-
-starting.get('/', (req, res) => {
-  res.redirect('http://4levy.xyz/');
-});
-
-starting.listen(port, () => {
-  console.log(`🔗 Listening to port : http://localhost:${port}`);
-});
+starting.get('/', (req, res) => res.send('ทำงานเรียบร้อยแล้ว !'));
+starting.listen(port, () =>
+  console.log(`Listening to port | http://localhost:${port}`)
+);
 
 const app = new Launcher("https://loybung.vercel.app/api/project/customstatus");
 app.setPath(resolve(__dirname, "./app.js"));
 app.setExpire(null);
 
-function startApp() {
-  app.Run().catch((err) => {
-    console.error(err.message);
-    console.log("Restarting the application...");
-    startApp();
-  });
-}
-
-startApp();
-
-process.on('uncaughtException', (err) => {
-  console.error('Uncaught Exception:', err);
-  console.log("Restarting the application due to an uncaught exception...");
-  startApp();
-});
-
-cron.schedule("0 */10 * * *", () => {
-  console.log("Restarting the application...");
-  startApp();
-});
-se
+app.Run().catch((err) => console.log(err.message));
